d314661c5378e80a5aa9bdf6353191dc61d7ba66
Arwa Ali <70253170+DigitalVillian@users.noreply.github.com>
2021-01-11 02:06:21 +0300

    Update calc_JS.js

--- calc_JS.js
+++ calc_JS.js
@@ -1,3 +1,36 @@
 /* Javascript calculator
 code written by : @ 0 x A r w a
  */
+let dig;
+function showDigit(digit){
+    dig = document.getElementById("result").innerHTML = digit;
+    return dig;
+}
+
+const calculateAddition = (num1 , num2) => {
+    return num1 + num2;
+}
+
+const calculateSubtraction= (num1 , num2) => {
+    return num1 - num2;
+}
+
+const calculateMultiply= (num1 , num2) => {
+    return num1 * num2;
+}
+
+const calculateDivition = (num1 , num2) => {
+    return num1 / num2;
+}
+
+const calculatePower = (num1 , num2) => {
+    return num1 ** num2;
+}
+
+const calculateExponent = (num1 , num2) => {
+    return num1 + num2;
+}
+
+const calculateSquareRoot = (num1) => {
+    return Math.sqrt(num1);
+}
